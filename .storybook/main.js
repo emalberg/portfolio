f8d84fc426c5cc8e596b5ec47e25eb4ece1c9f37
Erich Malberg <emalberg@live.com>
2024-10-22 21:04:59 -0600

    Basic next.js setup

---
+++ .storybook/main.js
@@ -0,0 +1,18 @@
+/** @type { import('@storybook/nextjs').StorybookConfig } */
+const config = {
+  stories: ['../**/*.mdx', '../**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+  addons: [
+    '@storybook/addon-onboarding',
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@chromatic-com/storybook',
+    '@storybook/addon-interactions',
+    '@storybook/addon-themes',
+    '@storybook/addon-a11y',
+  ],
+  framework: {
+    name: '@storybook/nextjs',
+    options: {},
+  },
+};
+export default config;
